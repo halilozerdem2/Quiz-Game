214311a3ce39a65287fe3469472064fafdd2bbe8
lilahmedrezo <i.h.ozerdem@gmail.com>
2022-05-03 21:22:25 +0300

    Last Commit

--- Quiz Game/Assets/GameManager.cs
+++ Quiz Game/Assets/GameManager.cs
@@ -8,66 +8,76 @@ public class GameManager : MonoBehaviour
 {
     public Question[] questions;
     private static List<Question> unansweredQuestion;
+
+//Text değişkenini kullanmak için unityengine ui kütüphanesini eklemelisiniz
     [SerializeField] private Text factText;
     [SerializeField] private Text aText;
     [SerializeField] private Text bText;
     [SerializeField] private Text cText;
     [SerializeField] private Text dText;
+    public int trueAnswers; // Bu değişken doğru sayısını tutuyor
+    public int falseAnswers;// Bu değişken yanlış sayısını tutuyor
+
         
 
-    private Question currentQuestion;
+    private Question currentQuestion; //Ekrandaki anlık soruyu temsil eder
      void Start() 
     {
-        if(unansweredQuestion==null || unansweredQuestion.Count==0)
+        if(unansweredQuestion==null || unansweredQuestion.Count==0) //Sorulmuş soruların soru havuzu listesinden çıkarılmasını sağlar
         {
             unansweredQuestion=questions.ToList<Question>();
         }
-        SetCurrentQuestion();
-        
-        
-    }
-/*
-    public void Soru_Hazirla() {
-        List<Question> myQuestionList = new List<Question>();
-        int soru_sayisi = 3;
-        int sayac = 0;
-        while(sayac < soru_sayisi) {
-            Question q = new Question("2+2",0,"4","2","3","5");
 
-        }
+        SetCurrentQuestion();//Random soru çağırma fonksiyonu
+        
+        
     }
-*/
+    
 
     void SetCurrentQuestion()
     {
-        if(unansweredQuestion.Count == 0) {
-            Debug.Log("Oyun bitti.");
+        if(unansweredQuestion.Count==0)//Sorular bittiği zaman olacak eventler buraya yazılır.
+        {
+            Debug.Log("Dogru soru sayiniz "+trueAnswers);
+            Debug.Log("Yanlis soru sayiniz "+falseAnswers);
+            Debug.Log("Oyun bitti!!");
             return;
         }
-
-        int randomQuestionIndex=Random.Range(0,unansweredQuestion.Count);
+        int randomQuestionIndex=Random.Range(0,unansweredQuestion.Count); //Rastgele soru üretme.
         currentQuestion=unansweredQuestion[randomQuestionIndex];
+        unansweredQuestion.RemoveAt(randomQuestionIndex);//Sorulmuş soruların soru havuzu listesinden çıkarılmasını sağlar
+
+        //alt satırdaki atamaların tamamı Uİ üzerinde Text değişkenleri ile string değişkenlerinin
+        //birbirine bağlanması için kurulmuştur.
+        factText.text=currentQuestion.fact; 
+        aText.text=currentQuestion.a;
+        bText.text=currentQuestion.b;
+        cText.text=currentQuestion.c;
+        dText.text=currentQuestion.d;
         
-        //Soru ve şıklar ekranda görüntülenir.
-        factText.text=currentQuestion.fact;
-        aText.text = currentQuestion.a;
-        bText.text = currentQuestion.b;
-        cText.text = currentQuestion.c;
-        dText.text = currentQuestion.d;
-        
-        unansweredQuestion.RemoveAt(randomQuestionIndex);
     }
 
-    public void checkAnswer(int choice_index) {
-        //a :0 b:1 c:2 d:3
-        bool is_correct = currentQuestion.trueAnswer == choice_index;
+    public void checkAnswer(int choice_index)// Buttonların onClick eventlarında kullanıdğımız fonksiyondur.
+    //buttonlara birer index ataması yapmak için ve cevapların doğru olup olmadığının kontrolünü yapmak için kullanılır.
+    {
+     //a=0 , b=1 , c=2 , d=3  
+     bool isCorrect =currentQuestion.trueAnswer==choice_index;
+    
+    if(isCorrect)
+    {
+        Debug.Log("Doğru!");
+        //doğru cevaplanmış soru sayısı sayacı arttırılır.
+        trueAnswers++;
 
-        if(is_correct){
-            Debug.Log("Doğru");
-        }
-        else Debug.Log("Yanlis");
-
-        SetCurrentQuestion();
+    } 
+    else {
+        Debug.Log("Yanlis!");
+        //yanlış cevaplanmış soru sayısı sayacı arttırılır.
+        falseAnswers++;
     }
+    //Sonraki sorunun çağırılması için çağırılır..
+
+     SetCurrentQuestion();
 
     }
+}
--- Quiz Game/Assets/Quesiton.cs
+++ Quiz Game/Assets/Quesiton.cs
@@ -3,18 +3,14 @@ public class Question
 {      
     public string fact;
     public int trueAnswer;
+    //Text değişkenlerinin atamsını yapmak için kullandığımız string değişkenler question scripti üzerinde tutuluyor.
     public string a;
     public string b;
     public string c;
     public string d;
-
-    public Question(string fact_p, int trueAnswer_p, string a_p, string b_p, string c_p, string d_p) {
-        fact = fact_p;
-        trueAnswer = trueAnswer_p;
-        a = a_p;
-        b = b_p;
-        c = c_p;
-        d = d_p;
-    }
+    // Aşşağıdaki yapıyı kullanmamıza gerek kalmadı çünkü oyun motoru üzerinden gerekli bağlantıları sağladık.
     
+
+    //Teşekkürler...
+  
 }
