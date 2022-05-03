faa478b7e3150591b9e2caf80003b5d5ecff3b0c
lilahmedrezo <i.h.ozerdem@gmail.com>
2022-05-03 19:03:43 +0300

    Done

--- Quiz Game/Assets/GameManager.cs
+++ Quiz Game/Assets/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using System.Linq;
 
@@ -7,6 +8,12 @@ public class GameManager : MonoBehaviour
 {
     public Question[] questions;
     private static List<Question> unansweredQuestion;
+    [SerializeField] private Text factText;
+    [SerializeField] private Text aText;
+    [SerializeField] private Text bText;
+    [SerializeField] private Text cText;
+    [SerializeField] private Text dText;
+        
 
     private Question currentQuestion;
      void Start() 
@@ -15,18 +22,52 @@ public class GameManager : MonoBehaviour
         {
             unansweredQuestion=questions.ToList<Question>();
         }
-        GetRandomQuestion();
+        SetCurrentQuestion();
         
-        Debug.Log(currentQuestion.fact+" "+ currentQuestion.trueAnswer);
         
     }
+/*
+    public void Soru_Hazirla() {
+        List<Question> myQuestionList = new List<Question>();
+        int soru_sayisi = 3;
+        int sayac = 0;
+        while(sayac < soru_sayisi) {
+            Question q = new Question("2+2",0,"4","2","3","5");
 
-    void GetRandomQuestion()
+        }
+    }
+*/
+
+    void SetCurrentQuestion()
     {
+        if(unansweredQuestion.Count == 0) {
+            Debug.Log("Oyun bitti.");
+            return;
+        }
+
         int randomQuestionIndex=Random.Range(0,unansweredQuestion.Count);
         currentQuestion=unansweredQuestion[randomQuestionIndex];
         
+        //Soru ve şıklar ekranda görüntülenir.
+        factText.text=currentQuestion.fact;
+        aText.text = currentQuestion.a;
+        bText.text = currentQuestion.b;
+        cText.text = currentQuestion.c;
+        dText.text = currentQuestion.d;
+        
         unansweredQuestion.RemoveAt(randomQuestionIndex);
     }
-   
-}
+
+    public void checkAnswer(int choice_index) {
+        //a :0 b:1 c:2 d:3
+        bool is_correct = currentQuestion.trueAnswer == choice_index;
+
+        if(is_correct){
+            Debug.Log("Doğru");
+        }
+        else Debug.Log("Yanlis");
+
+        SetCurrentQuestion();
+    }
+
+    }
--- Quiz Game/Assets/Quesiton.cs
+++ Quiz Game/Assets/Quesiton.cs
@@ -3,5 +3,18 @@ public class Question
 {      
     public string fact;
     public int trueAnswer;
+    public string a;
+    public string b;
+    public string c;
+    public string d;
+
+    public Question(string fact_p, int trueAnswer_p, string a_p, string b_p, string c_p, string d_p) {
+        fact = fact_p;
+        trueAnswer = trueAnswer_p;
+        a = a_p;
+        b = b_p;
+        c = c_p;
+        d = d_p;
+    }
     
 }
